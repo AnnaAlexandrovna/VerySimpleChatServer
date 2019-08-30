a3f122edee50248c3ee694ba08496377b54d4608
AnnaAlexandrovna <salatik339@mail.ru>
2019-08-30 22:15:09 +0300

    Initial commit

---
+++ src/com/company/VerySimpleChatServer.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class VerySimpleChatServer {
+
+    ArrayList clientOutputSttreams;
+
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket sock;
+
+        public ClientHandler(Socket clientSocket) {
+            try {
+                sock = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        public void run(){
+            String message;
+            try {
+                while ((message = reader.readLine())!= null){
+                    System.out.println("read" + message);
+                    tellEveryone(message);
+                }
+            }catch (Exception ex){ex.printStackTrace();}
+        }
+    }
+
+    public static void main(String[] args) {
+        new VerySimpleChatServer().go();
+    }
+
+    public void go(){
+        clientOutputSttreams = new ArrayList();
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            while (true){
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputSttreams.add(writer);
+
+                Thread thread = new Thread(new ClientHandler(clientSocket));
+                thread.start();
+                System.out.println("got a connection");
+            }
+        }catch (Exception ex){ex.printStackTrace();}
+    }
+
+    public void tellEveryone(String message){
+        Iterator iterator = clientOutputSttreams.iterator();
+        while (iterator.hasNext()){
+            try {
+                PrintWriter writer = (PrintWriter) iterator.next();
+                writer.println(message);
+                writer.flush();
+            }catch (Exception ex){ex.printStackTrace();}
+        }
+    }
+}
